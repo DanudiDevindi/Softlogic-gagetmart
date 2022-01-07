3399bb2aa2e1bc2a9601ad42af2653ac78a4c036
Danudi <danusilva1997@gmail.com>
2022-01-07 12:05:09 +0530

    initial commit

---
+++ src/main/java/com/bolton/softlogic_gagetmart/SoftlogicGagetmartApplication.java
@@ -0,0 +1,13 @@
+package com.bolton.softlogic_gagetmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftlogicGagetmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftlogicGagetmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bolton/softlogic_gagetmart/SoftlogicGagetmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolton.softlogic_gagetmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftlogicGagetmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
